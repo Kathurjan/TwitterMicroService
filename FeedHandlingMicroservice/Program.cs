fc986b125d4f4c5882d66c458044a2a6a0ef572a
Kathurjan <kath097c@easv365.dk>
2024-03-22 13:43:07 +0100

    Fix: authrize swagger stuff for jwt tokens added

--- FeedHandlingMicroservice/Program.cs
+++ FeedHandlingMicroservice/Program.cs
@@ -6,10 +6,11 @@ using FeedHandlingMicroservice.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<DbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("FeedDb")));
+builder.Services.AddDbContext<PostDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("PostDb")));
 builder.Services.AddControllers();
 var config = new MapperConfiguration(conf =>
 {
@@ -19,7 +20,8 @@ var config = new MapperConfiguration(conf =>
 
 var mapper = config.CreateMapper();
 builder.Services.AddSingleton(mapper);
-
+builder.Services.AddScoped<IPostRepo, PostRepo>();
+builder.Services.AddScoped<IPostService, PostService>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -34,9 +36,36 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IPostRepo, PostRepo>();
-builder.Services.AddScoped<IPostService, PostService>();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header using the Bearer scheme. Example: 'Authorization: Bearer {token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
+
 
 var app = builder.Build();
 
