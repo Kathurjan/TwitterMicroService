b3013400eb7f22080a866239ddf7de83d4549c97
Kathurjan <kath097c@easv365.dk>
2024-03-23 16:57:42 +0100

    Feat: crud methods for hashtag added in repo

--- FeedHandlingMicroservice/DataAccess/Interface/IHashTagRepo.cs
+++ FeedHandlingMicroservice/DataAccess/Interface/IHashTagRepo.cs
@@ -1,6 +1,11 @@
-﻿namespace FeedHandlingMicroservice.DataAccess;
+﻿using FeedHandlingMicroservice.Models;
+
+namespace FeedHandlingMicroservice.DataAccess;
 
 public interface IHashTagRepo
 {
-    
+    Task<Hashtag> CreateNewHashtag(Hashtag hashtag);
+    Task<Hashtag> GetHashTagById(int id);
+    Task<List<Hashtag>> GetAllHashTags();
+    Task<Hashtag> DeleteHashtagById(int id); // not sure if this is needed?
 }
--- FeedHandlingMicroservice/DataAccess/Repo/HashTagRepo.cs
+++ FeedHandlingMicroservice/DataAccess/Repo/HashTagRepo.cs
@@ -1,6 +1,67 @@
-﻿namespace FeedHandlingMicroservice.DataAccess;
+﻿using FeedHandlingMicroservice.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FeedHandlingMicroservice.DataAccess;
 
 public class HashTagRepo : IHashTagRepo
 {
-    
+    private readonly PostDbContext _dbContext;
+
+    public HashTagRepo(PostDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public async Task<Hashtag> CreateNewHashtag(Hashtag hashtag)
+    {
+        try
+        {
+            await _dbContext.Hashtags.AddAsync(hashtag);
+            await _dbContext.SaveChangesAsync();
+            return hashtag;
+        }
+        catch (Exception e)
+        {
+           
+            throw new Exception("Something went wrong in Repo CreateNewHashtag method " + e);
+        }
+
+    }
+
+    public Task<Hashtag> GetHashTagById(int id)
+    {
+        
+        try
+        {
+            return _dbContext.Hashtags.FirstOrDefaultAsync(h => h.Id == id)!;
+        }
+        catch (Exception e)
+        {
+            
+            throw new Exception("Something went wrong in Repo GetHashTagById method " + e);
+        }
+        
+    }
+
+    public Task<List<Hashtag>> GetAllHashTags()
+    {
+        try
+        {
+            return _dbContext.Hashtags.ToListAsync();
+        }
+        catch (Exception e)
+        {
+            throw new Exception("Something went wrong in Repo GetAllHashTags method " + e);
+        }
+       
+    }
+
+    public async Task<Hashtag> DeleteHashtagById(int id)
+    {
+        var hashTagToDelete = _dbContext.Hashtags.Find(id) ?? throw new KeyNotFoundException("No such hashtag found");
+        _dbContext.Hashtags.Remove(hashTagToDelete);
+        await _dbContext.SaveChangesAsync();
+        return hashTagToDelete;
+    }
 }
